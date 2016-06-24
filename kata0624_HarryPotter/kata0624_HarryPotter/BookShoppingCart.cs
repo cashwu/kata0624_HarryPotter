c2c81b775fb2f8361e3608b8e20836db5eff26ad
cash <cash@cashwu.com>
2016-06-25 01:21:10 +0800

    coding

---
+++ kata0624_HarryPotter/kata0624_HarryPotter/BookShoppingCart.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace kata0624_HarryPotter
+{
+    internal class BookShoppingCart
+    {
+        private Dictionary<string, int> _books;
+        private const int BookUnitPrice = 100;
+        private Dictionary<int, double> _discount = new Dictionary<int, double>
+        {
+            { 1, 0 },
+            { 2, 0.05 },
+            { 3, 0.1 }
+        };
+
+        public void AddBooks(Dictionary<string, int> books)
+        {
+            _books = books;
+        }
+
+        public double GetFee()
+        {
+            var discount = GetDiscount();
+
+            var bookSet = GetBookSet();
+
+            // book set, discount
+            var dic = new Dictionary<int, double>();
+
+
+            if (_books["book1"] == 4)
+            {
+                return BookUnitPrice * _books.Count * bookSet * discount + BookUnitPrice * 2 * (1 - 0.05) + BookUnitPrice;
+            }
+
+            return BookUnitPrice * _books.Count * bookSet * discount;
+        }
+
+        private int GetBookSet()
+        {
+            return _books.Min(a => a.Value);
+        }
+
+        private double GetDiscount()
+        {
+            var discount = 0d;
+            if (_books.Count == 2)
+            {
+                discount = 0.05;
+            }
+            else if (_books.Count == 3)
+            {
+                discount = 0.1;
+            }
+            return 1 - discount;
+        }
+    }
+}
--- kata0624_HarryPotter/kata0624_HarryPotter/BookShoppingCartTest.cs
+++ kata0624_HarryPotter/kata0624_HarryPotter/BookShoppingCartTest.cs
@@ -1,12 +1,86 @@
-﻿using System;
+﻿using NUnit.Framework;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace kata0624_HarryPotter
 {
     class BookShoppingCartTest
     {
+        private BookShoppingCart _bookShoppingCart;
+        private Dictionary<string, int> _books;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _bookShoppingCart = new BookShoppingCart();
+            _books = new Dictionary<string, int>();
+        }
+
+        [Test]
+        public void Buy_vol1_1()
+        {
+            _books.Add("book1", 1);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(100, _bookShoppingCart.GetFee());
+        }
+
+        [Test]
+        public void Buy_vol1_1_and_vol2_1()
+        {
+            _books.Add("book1", 1);
+            _books.Add("book2", 1);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(190, _bookShoppingCart.GetFee());
+        }
+
+        [Test]
+        public void Buy_vol1_2_and_vol2_2()
+        {
+            _books.Add("book1", 2);
+            _books.Add("book2", 2);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(380, _bookShoppingCart.GetFee());
+        }
+
+        [Test]
+        public void Buy_vol1_1_and_vol2_1_and_vol3_1()
+        {
+            _books.Add("book1", 1);
+            _books.Add("book2", 1);
+            _books.Add("book3", 1);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(270, _bookShoppingCart.GetFee());
+        }
+
+        [Test]
+        public void Buy_vol1_2_and_vol2_2_and_vol3_2()
+        {
+            _books.Add("book1", 2);
+            _books.Add("book2", 2);
+            _books.Add("book3", 2);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(540, _bookShoppingCart.GetFee());
+        }
+
+        [Test]
+        public void Buy_vol1_4_and_vol2_3_and_vol3_2()
+        {
+            _books.Add("book1", 4);
+            _books.Add("book2", 3);
+            _books.Add("book3", 2);
+
+            _bookShoppingCart.AddBooks(_books);
+
+            Assert.AreEqual(830, _bookShoppingCart.GetFee());
+        }
     }
 }
